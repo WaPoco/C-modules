8d12a98afac1b01fef916e95e2f269f709a6cdf5
WaPoco1994 <vasili-pogorelov@web.de>
2025-08-29 18:09:32 +0200

    Started with cpp01 ex01

---
+++ cpp01/ex01/Zombie.cpp
@@ -0,0 +1,26 @@
+#include "Zombie.hpp"
+
+Zombie::~Zombie()
+{
+    std::cout << _name << std::endl;
+}
+
+Zombie::Zombie(std::string name)
+{
+    _name = name;
+    announce();
+};
+
+void    Zombie::announce()
+{
+    std::cout << _name << ": " << "BraiiiiiiinnnzzzZ..." << std::endl;
+}
+Zombie  *Zombie::newZombie(std::string name)
+{
+    Zombie *newZombie = new Zombie(name);
+    return (newZombie);
+}
+void    Zombie::randomChump(std::string name)
+{
+    Zombie randomZombie (name);
+}
---
+++ cpp01/ex01/main.cpp
@@ -0,0 +1,9 @@
+#include "Zombie.hpp"
+
+int main()
+{
+    Zombie A ("Peter");
+    A.newZombie("B");
+    A.randomChump("FOO");
+    return (0);
+}
