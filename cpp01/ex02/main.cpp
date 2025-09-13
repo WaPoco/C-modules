01b937591a2abe8461fda6f5ba71292e633b2e84
WaPoco1994 <vasili-pogorelov@web.de>
2025-09-13 13:44:54 +0200

    Changed cpp files from ex01

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,14 +6,14 @@ int main(void)
     std::string str = "HI THIS IS BRAIN";
     std::string *stringPTR;
     stringPTR = &str;
-    std::string stringREF = str;
+    std::string &stringREF = str;
 
     std::cout << "The memory address of a string " << &str << std::endl;
-    std::cout << "The memory address of a pointer to the string " << stringPTR << std::endl;
+    std::cout << "The memory address of a pointer to a string " << stringPTR << std::endl;
     std::cout << "The memory address of a reference to a string " <<  &stringREF << std::endl;
 
     std::cout << "The value of a string " << str << std::endl;
-    std::cout << "The value of " << *stringPTR << std::endl;
+    std::cout << "The value of a pointer to a string " << *stringPTR << std::endl;
     std::cout << "The value of a reference to a string " << stringREF << std::endl;
     return (0);
 }
