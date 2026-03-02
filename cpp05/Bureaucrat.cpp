4b34c72153d70540141f2f98e84afa81c207ccdd
WaPoco1994 <vasili-pogorelov@web.de>
2026-03-02 20:50:49 +0100

    New exception

--- cpp05/Bureaucrat.cpp
+++ cpp05/Bureaucrat.cpp
@@ -1,14 +1,20 @@
+#include <iostream>
 #include "Bureaucrat.hpp"
 #include "GradeTooLowException.hpp"
+#include "GradeTooHighException.hpp"
 
-Bureaucrat::Bureaucrat(const std::string &name, int grade)
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : name(name), grade(grade)
 {
     try
     {
         if (grade < 1)
-            throw Bureaucrat::GradeTooLowException;
+            throw GradeTooLowException();
         else if (grade > 150)
-            throw Bureaucrat::GradeTooHighException;
+            throw GradeTooHighException();
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what() << '\n';
     }
 }
 
@@ -18,19 +24,25 @@ Bureaucrat::~Bureaucrat()
 
 const std::string &Bureaucrat::getName() const
 {
-    // TODO: insert return statement here
+    return name;
 }
 
 int Bureaucrat::getGrade() const
 {
-    return 0;
+    return grade;
 }
 
 void Bureaucrat::incrementGrade()
 {
+    if (grade - 1 < 1)
+        throw GradeTooHighException();
+    grade--;
 }
 
 void Bureaucrat::decrementGrade()
 {
+    if (grade + 1 > 150)
+        throw GradeTooLowException();
+    grade++;
 }
 
---
+++ cpp05/main.cpp
@@ -0,0 +1,7 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+    Bureaucrat a("Vasili", 1);
+    return 0;
+}
