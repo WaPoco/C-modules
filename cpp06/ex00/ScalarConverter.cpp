c3802c52e75603edf6b32922def43d3c8135f3ab
WaPoco1994 <vasili-pogorelov@web.de>
2026-04-17 18:51:57 +0200

    Added new functions to convert

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,4 +1,6 @@
 #include "ScalarConverter.hpp"
+#include <string>
+
 
 ScalarConverter::ScalarConverter()
 {
@@ -9,7 +11,24 @@ ScalarConverter::~ScalarConverter()
 {
 }
 
-void ScalarConverter::convert()
+void ScalarConverter::convert(std::string str)
 {
     
+}
+
+void ScalarConverter::isInt()
+{
+}
+
+void ScalarConverter::isChar()
+{
+
+}
+
+void ScalarConverter::isDouble()
+{
+}
+
+void ScalarConverter::isFloat()
+{
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -1,12 +1,19 @@
 # ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
+# include <string>
 
 class ScalarConverter
 {
+    private:
+        std::string input;
     public:
         ScalarConverter();
         ~ScalarConverter();
-        static void convert();
+        static void convert(std::string str);
+        void isInt();
+        void isChar();
+        void isDouble();
+        void isFloat();
 };
 
 #endif
