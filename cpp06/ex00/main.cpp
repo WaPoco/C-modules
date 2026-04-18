e10169b77f096c4d072a641f759c5b512e72836b
WaPoco1994 <vasili-pogorelov@web.de>
2026-04-18 19:07:33 +0200

    Need to check type and then cast

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,19 +1,27 @@
 #include "ScalarConverter.hpp"
 #include "exception"
 #include "iostream"
+#include <cstdlib>
+#include <cerrno>
 
 int main(int arg, char **args)
 {
-    //ScalarConverter a;
-    int a = 1;
-    try
-    {
-        double b = static_cast<double>(args[1]);
+    std::string s = args[1];
+    char* end;
 
+
+    float i = std::strtof(s.c_str(), &end);
+    double d = std::strtod(s.c_str(), &end);
+
+    std::cout << i << ", " << d << std::endl;
+    errno = 0;
+    float val = std::strtof(s.c_str(), &end);
+
+    if (end == s) {
+        std::cout << "No conversion\n";
+    } else if (errno == ERANGE) {
+        std::cout << "Out of range\n";
+    } else {
+        std::cout << "Value: " << val << "\n";
     }
-    catch (std::exception &e)
-    {
-        std::cout << e.what();
-    }
-    return (1);
 }
