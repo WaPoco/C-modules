514d87b1175d6bae02002b3461d90ac222cb62db
WaPoco1994 <vasili-pogorelov@web.de>
2026-07-30 16:56:27 +0200

    New idea

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -1,18 +1,17 @@
 #include"Span.hpp"
 
-
 int main()
 {
     {
         Span sp = Span(100);
-        //std::vector<int> v;
-        //v = {1, 1, 30, 4, 55, 56};
+        std::vector<int> v;
+        v = {1, 1, 30, 4, 55, 56};
         sp.addNumber(1);
         sp.addNumber(1);
         sp.addNumber(3);
         sp.addNumber(4);
         sp.addNumber(5);
-        //sp.addMultipleNumbers(v.begin(), v.end());
+        sp.addMultipleNumbers(v.begin(), v.end());
         std::cout << sp.shortestSpan() << std::endl;
         std::cout << sp.longestSpan() << std::endl;
     }
---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,10 @@
+#include <stack>
+#include <vector>
+
+int main()
+{
+    std::stack<int> s;
+    std::vector<int> d = {1,2};
+    std::vector<int>::iterator i = d.begin();
+    return (0);
+}
