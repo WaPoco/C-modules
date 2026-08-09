14ccf70c472e7ecfc581205845686a8192ffaab7
WaPoco1994 <vasili-pogorelov@web.de>
2026-08-09 18:37:00 +0200

    Changed MutantStack class

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -24,20 +24,17 @@ class MutantStack : public std::stack<T, Container>
 };
 
 template <typename T, typename Container>
-MutantStack<T, Container>::MutantStack()
-    : std::stack<T, Container>()
+MutantStack<T, Container>::MutantStack(): std::stack<T, Container>()
 {
 }
 
 template <typename T, typename Container>
-MutantStack<T, Container>::MutantStack(const MutantStack &other)
-    : std::stack<T, Container>(other)
+MutantStack<T, Container>::MutantStack(const MutantStack &other): std::stack<T, Container>(other)
 {
 }
 
 template <typename T, typename Container>
-MutantStack<T, Container> &
-MutantStack<T, Container>::operator=(const MutantStack &other)
+MutantStack<T, Container> &MutantStack<T, Container>::operator=(const MutantStack &other)
 {
     if (this != &other)
         std::stack<T, Container>::operator=(other);
@@ -50,28 +47,24 @@ MutantStack<T, Container>::~MutantStack()
 }
 
 template <typename T, typename Container>
-typename MutantStack<T, Container>::iterator
-MutantStack<T, Container>::begin()
+typename MutantStack<T, Container>::iterator MutantStack<T, Container>::begin()
 {
     return this->c.begin();
 }
 
 template <typename T, typename Container>
-typename MutantStack<T, Container>::iterator
-MutantStack<T, Container>::end()
+typename MutantStack<T, Container>::iterator MutantStack<T, Container>::end()
 {
     return this->c.end();
 }
 
-template <typename T, typename Container>
-typename MutantStack<T, Container>::const_iterator
+template <typename T, typename Container> typename MutantStack<T, Container>::const_iterator
 MutantStack<T, Container>::begin() const
 {
     return this->c.begin();
 }
 
-template <typename T, typename Container>
-typename MutantStack<T, Container>::const_iterator
+template <typename T, typename Container> typename MutantStack<T, Container>::const_iterator
 MutantStack<T, Container>::end() const
 {
     return this->c.end();
